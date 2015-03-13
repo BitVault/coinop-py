9b9b0980f2012572fa92c5de6377adbce1d5a2c5
Matt Smith <matt.the.smith@gmail.com>
2015-03-13 10:45:06 -0700

    bump to 0.1.1 for public availability

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='coinop',
-      version='0.1.0',
+      version='0.1.1',
       description='Crypto-currency conveniences',
       url='http://github.com/GemHQ/coinop-py',
       author='Matt Smith',
