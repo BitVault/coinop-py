bd01bca12ac5687356432923d503dc9f3b3ac09e
Matt Smith <matt.the.smith@gmail.com>
2015-03-24 16:15:09 -0700

    Release 0.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,10 @@
 
+0.1.3 / 2015-03-24
+==================
+
+  *  Refactor seed/secret storage
+  *  Minor bug fixes and enhancements
+
 0.1.2 / 2015-03-16
 ==================
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='coinop',
-      version='0.1.2',
+      version='0.1.3',
       description='Crypto-currency conveniences',
       url='http://github.com/GemHQ/coinop-py',
       author='Matt Smith',
